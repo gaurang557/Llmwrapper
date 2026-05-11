e914c52e46a950308bff4379455b2e188e763f14
gaurang <gaurangagarwal557@gmail.com>
2026-05-11 22:37:00 +0530

    initializing spring application

---
+++ llmwrapper/src/test/java/com/gaurang/llmwrapper/LlmwrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurang.llmwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmwrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
